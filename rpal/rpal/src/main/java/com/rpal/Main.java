59e6eccc7372639c01d810255fced36a91861a55
Geesan Munasinghe <sugithgeesan@gmail.com>
2025-05-15 19:03:08 +0530

    Main flow setup

--- rpal/rpal/src/main/java/com/rpal/Main.java
+++ rpal/rpal/src/main/java/com/rpal/Main.java
@@ -1,14 +1,29 @@
 package com.rpal;
 
 import java.io.File;
+import java.util.List;
+
+import com.rpal.lex.LexicalAnalyzer;
+import com.rpal.lex.Token;
+import com.rpal.parser.AST;
+import com.rpal.parser.Parser;
 
 public class Main {
     public static void main(String[] args) {
+        // Check if a filename is given
         if (args.length < 1) {
             System.out.println("Usage: java com.rpal.Main <sourcefile.rpal>");
             return;
         }
+        // Generate the lexical analyser targeting given file
         File file = new File(args[0]);
-        System.out.println("Interpreting file : "+file.getName());
+        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(file);
+        // Tokanize the input
+        List<Token> tokenList = lexicalAnalyzer.getTokenList();
+        // Build AST
+        Parser parser = new Parser(tokenList);
+        AST tree = parser.buildAst();
+        // Standardise AST
+        tree.standardize();
     }
 }
---
+++ rpal/rpal/src/main/java/com/rpal/lex/Token.java
@@ -0,0 +1,27 @@
+package com.rpal.lex;
+
+public class Token {
+    public String getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getValue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setType(String type) {
+        // TODO Auto-generated method stub
+    }
+
+    public void setValue(String value) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
